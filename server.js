5c2d59ad9da1590d87966737dc363a6f8b8af934
Humberto Maquilon <humberto@Humbertos-MBP.attlocal.net>
2018-05-29 11:22:04 -0400

    Added Post

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // BASE SETUP
 // =============================================================================
-var mongoose   = require('mongoose');
+var mongoose = require('mongoose');
 //mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
 mongoose.connect('mongodb://localhost:27017/labels')
 
 var Album = require('./app/models/album');
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');        // call express
+var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
@@ -26,7 +26,7 @@ var router = express.Router();              // get an instance of the express Ro
 // We can do validations to make sure that everything coming from a request is 
 // safe and sound. We can throw errors here in case something is wrong. We can 
 // do some extra logging for analytics or any statistics we'd like to keep.
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
     // do logging
     console.log('Request URL -->', req.method, req.url);
     next(); // make sure we go to the next routes and don't stop here
@@ -38,15 +38,16 @@ router.use(function(req, res, next) {
 // });
 
 
-// more routes for our API will happen here
 
+
+// Routes for our API will happen here
 
 
 // get all the albums (accessed at GET http://localhost:8080/api/albums)
 router.route('/albums')
-    .get(function(req,res) {
-        Album.find(function(err, albums) {
-            if (err) 
+    .get(function (req, res) {
+        Album.find(function (err, albums) {
+            if (err)
                 res.send(err);
             res.json(albums)
         });
@@ -54,35 +55,57 @@ router.route('/albums')
 
 
 
+
 // get the album with that id (accessed at GET http://localhost:8080/api/albums/:album_id)
 router.route('/albums/:album_id')
-    .get(function(req,res) {
-        Album.findById(req.params.album_id, function(err, album) {
+    .get(function (req, res) {
+        Album.findById(req.params.album_id, function (err, album) {
             if (err)
                 res.send(err);
+                
             res.json(album)
         });
     });
 
 
 
-    
-// update the album with this id (accessed at PUT http://localhost:8080/api/albums/:album_id)
-router.route('/albums/:album_id')
-    .put(function(req,res) {
-        Album.findById(req.params.album_id, function(err,album) {
+
+// create a album (accessed at POST http://localhost:8080/api/albums
+router.route('/albums')
+    .post(function (req, res) {
+        var album = new Album();    // Create a new instance of the Album model
+        album.labelName = req.body.labelName;
+        album.year = req.body.year;
+
+        // Save the album and check for errors
+        album.save(function (err) {
             if (err)
                 res.send(err);
-            
+
+            res.json({ message: 'Album created!' })
+        });
+    });
+
+
+
+
+
+// update the album with this id (accessed at PUT http://localhost:8080/api/albums/:album_id)
+router.route('/albums/:album_id')
+    .put(function (req, res) {
+        Album.findById(req.params.album_id, function (err, album) {
+            if (err)
+                res.send(err);
+
             album.labelName = req.body.labelName;  // Update the album info
-            album.year      = req.body.year;
+            album.year = req.body.year;
 
             // Save the album
-            album.save(function(err) {
+            album.save(function (err) {
                 if (err)
                     res.send(err);
-                
-                res.json({ message: 'Album updated!'})
+
+                res.json({ message: 'Album updated!' })
             })
         })
     })
